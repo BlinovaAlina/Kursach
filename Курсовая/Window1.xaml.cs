48470e754b86017d1554b9c143ea008799ad4241
BlinovaAlina <alina.blinova.asus.pad@gmail.com>
2025-01-04 19:15:30 +0700

    433456

--- Курсовая/Window1.xaml.cs
+++ Курсовая/Window1.xaml.cs
@@ -37,8 +37,6 @@ namespace Курсовая
             RussianWord_textbox.Text = "";
             
             list_new = new List<AddWords>();
-
-            string nekaya_stroka = "";
         }
 
         private void AddMore_Click(object sender, RoutedEventArgs e)
